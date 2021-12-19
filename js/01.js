e7ae84ce5a300e448e12d790628b53324554546e
EunkyungYu <eunkyung211@naver.com>
2021-12-19 14:40:58 +0900

    03 jfkjdksf

--- js/01.js
+++ js/01.js
@@ -1,1 +1,35 @@
-alert("Hello World");
+//alert("Hello World");
+const PI = 3.14;
+let myAge = 27;
+const myName = "유은경";
+let myHeight = 180;
+let myWeight = 80;
+let meterHeight = myHeight/100;
+let biman = 80/(meterHeight*meterHeight);
+
+// 18.5보다 작으면 저체중
+// 23보다 작으면 정상
+// 25보다 작으면 과체중
+// 30보다 작으명 비만
+// 30보다 크면 고도비만
+// 문 -> statement 들은 연산을 하지 않고 분개를 해주는 것.
+// 문 -> 조건문(if, else, switch), 반복문(for, while)
+/*if(biman>18.5) {
+
+} else if(biman) {
+
+} else if(biman) {
+    
+} else if(biman) {
+    
+} else {
+    
+}*/
+
+// boolean - true, false
+console.log(10<5);
+
+console.log(biman);
+
+console.log(myName);
+console.log(myAge);
