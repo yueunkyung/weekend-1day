dd60620fa47e43dd4836d4e51ef02cdd008e810c
EunkyungYu <eunkyung211@naver.com>
2021-12-19 17:37:13 +0900

    06 아래쪽 출력완성

--- js/biman.js
+++ js/biman.js
@@ -2,6 +2,9 @@
 const inputHeight = document.querySelector("#height");
 const inputWeight = document.querySelector("#weight");
 const btnResult = document.querySelector("#btnResult");
+const txtResult = document.querySelector("#txtResult");
+const txtResultNum = document.querySelector("#txtResult .num");
+const txtResultTxt = document.querySelector("#txtResult .txt");
 
 /*
 const now = new Date();
@@ -43,12 +46,25 @@ btnResult.addEventListener("click", function(){
 
     let meterHeight = myHeight/100;
     let biman = myWeight/(meterHeight*meterHeight);
-
     if(biman < 18.5) {
-        alert("저체중 입니다. 삼겹살을 권합니다.");
+        // alert("저체중 입니다. 삼겹살을 권합니다.");
+        txtResult.style.display="flex";
+        txtResultNum.textContent = biman;
+        txtResultTxt.textContent = "저체중 입니다. 삼겹살을 권합니다.";
+        
     } else if(biman < 23) {
-        alert("정상 입니다. 이대로 쭉...");
+        // alert("정상 입니다. 이대로 쭉...");
+        txtResult.style.display="flex";
+        txtResultNum.textContent = biman;
+        txtResultTxt.textContent = "정상 입니다. 이대로 쭉...";
     } else if(biman < 25) {    
-        alert("과체중 입니다. 좀만 덜 드시면 무병장수...");
+        // alert("과체중 입니다. 좀만 덜 드시면 무병장수...");
+        txtResult.style.display="flex";
+        txtResultNum.textContent = biman;
+        txtResultTxt.textContent = "과체중 입니다. 좀만 덜 드시면 무병장수...";
+    } else {
+        txtResult.style.display="flex";
+        txtResultNum.textContent = biman;
+        txtResultTxt.textContent = "언제 죽어도 모른다는....";
     }
 });
