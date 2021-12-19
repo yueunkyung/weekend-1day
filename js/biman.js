bcc69aaa6478ace21477f1d29c76e88969a2d600
EunkyungYu <eunkyung211@naver.com>
2021-12-19 17:54:48 +0900

    07 Math round (반올리) 사용

--- js/biman.js
+++ js/biman.js
@@ -30,6 +30,13 @@ jegob(3);
 // console.log(inputHeight);
 // console.log(inputWeight);
 
+function showAlertTxt(msg, _biman){
+    
+    txtResult.style.display = "flex";
+    txtResultNum.textContent = Math.round(_biman*100)/100;
+    txtResultTxt.textContent = msg;
+}
+
 btnResult.addEventListener("click", function(){
     let myHeight = inputHeight.value;
     let myWeight = inputWeight.value;
@@ -46,25 +53,17 @@ btnResult.addEventListener("click", function(){
 
     let meterHeight = myHeight/100;
     let biman = myWeight/(meterHeight*meterHeight);
+
     if(biman < 18.5) {
         // alert("저체중 입니다. 삼겹살을 권합니다.");
-        txtResult.style.display="flex";
-        txtResultNum.textContent = biman;
-        txtResultTxt.textContent = "저체중 입니다. 삼겹살을 권합니다.";
-        
+        showAlertTxt("저체중 입니다. 삼겹살을 권합니다.", biman);
     } else if(biman < 23) {
         // alert("정상 입니다. 이대로 쭉...");
-        txtResult.style.display="flex";
-        txtResultNum.textContent = biman;
-        txtResultTxt.textContent = "정상 입니다. 이대로 쭉...";
+        showAlertTxt("정상 입니다. 이대로 쭉...", biman);
     } else if(biman < 25) {    
         // alert("과체중 입니다. 좀만 덜 드시면 무병장수...");
-        txtResult.style.display="flex";
-        txtResultNum.textContent = biman;
-        txtResultTxt.textContent = "과체중 입니다. 좀만 덜 드시면 무병장수...";
+        showAlertTxt("과체중 입니다. 좀만 덜 드시면 무병장수...", biman);
     } else {
-        txtResult.style.display="flex";
-        txtResultNum.textContent = biman;
-        txtResultTxt.textContent = "언제 죽어도 모른다는....";
+        showAlertTxt("언제 죽어도 모른다는....", biman);
     }
 });
